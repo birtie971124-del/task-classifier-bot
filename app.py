8f4fd94ba09892c3e2a605c37ed15e84d2b9020a
birtie971124-del <birtie971124@gmail.com>
2025-12-25 22:38:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,13 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_text = event.message.text
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="✅ 我真的有收到你傳的訊息")
+        TextSendMessage(text=f"我收到你的訊息了：{user_text}")
     )
+
+
+if __name__ == "__main__":
+    app.run()
